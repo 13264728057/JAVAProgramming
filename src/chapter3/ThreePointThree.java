6de7226b816a65018d9d091099fe17f4afa83728
13264728057 <1192965000@qq.com>
2018-11-27 20:54:59 +0800

    星期二

--- src/chapter3/ThreePointThree.java
+++ src/chapter3/ThreePointThree.java
@@ -6,4 +6,10 @@ package chapter3;
  * @Description:
  */
 public class ThreePointThree {
+   public static void main(String[] args) {
+      boolean t = true;
+      boolean f = false;
+      System.out.println((t || t) && f);
+      System.out.println(t && t || t);
+   }
 }
